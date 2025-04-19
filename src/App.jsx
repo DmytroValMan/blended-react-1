e5375b052b13a126ba9fa93f39ccf6f7e46a81a5
DmytroValMan <dmitri.m2011@gmail.com>
2025-04-19 22:40:36 +0300

    complete all 4 tasks

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,26 @@ import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
 
+import article from './data/article.json';
+import stats from './data/stats.json';
+import list from './data/forbes.json';
+import transactions from './data/transactions.json';
+
 export const App = () => {
   return (
     <Section>
       <Container>
         <Heading title="Task 1 Blog Card" bottom />
-        <BlogCard />
+        <BlogCard article={article} />
 
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
+        <Statistics title="Main Statistics" stats={stats} />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={list} />
 
         <Heading title="Task 4 Crypto history" top bottom />
-        <CryptoHistory />
+        <CryptoHistory transactions={transactions} />
       </Container>
     </Section>
   );
